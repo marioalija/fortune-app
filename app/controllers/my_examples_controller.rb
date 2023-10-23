3ad60eb83e412d28f3f4f9e72df7fced3a2aec48
marioalija <marioalija25@gmail.com>
2023-10-23 10:41:09 -0500

    -m

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,13 +1,12 @@
 class MyExamplesController < ApplicationController
   def fortune_teller
     strings = ["You will win the lottery", "No, im just kidding", "Don't amswer your next phone call"]
-    fortune = strings[rand(strings.length)]
+    fortune = strings.sample
     render json: { "Your fortune is:" => "#{fortune}" }
   end
 
   def lotto_numbers
-    numbers = (1..60).to_a
-    lotto_numbers = []
+    numbers = (1..60)
     6.times do
       lotto_numbers << numbers[rand(numbers.length)]
     end
